68cb9890595019f045d6a9f09ad172a8d0a04b6c
Nora Terbocs <nora.terbocs@gmail.com>
2023-10-12 16:20:34 +0200

    Added new skills

--- src/pages/techpage/Tech.js
+++ src/pages/techpage/Tech.js
@@ -11,11 +11,11 @@ export const Tech = () => {
       <BackgroundColor backgroundColor="#edfff1" />
       <SectionTitle>Tech</SectionTitle>
       <Paragraph lineHeight="40px">HTML, CSS, <Highlighter backgroundColor="#EB5577">JavaScript,</Highlighter> ES6, JSX,
-        <Highlighter backgroundColor="#0B24F5">React, </Highlighter>
+        <Highlighter backgroundColor="#0B24F5">React,</Highlighter> Next.js,
                 React Hooks, React Router, Styled Components,
                 Redux, Node.js, Express API,
                 Mongo DB, Web Accessibility,
-                API:s,
+                API:s, Tailwind CSS,
         <Highlighter backgroundColor="#2483E0">mob-programming,</Highlighter> pair-programming, Github.
       </Paragraph>
     </SectionContainer>
